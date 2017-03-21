7fc37daa8f3a6f79d60be768d2b437f3be5210cb
Landon Patmore <landon.patmore@gmail.com>
2017-03-21 14:36:14 -0400

    made menu collabsable on mobile

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,9 @@
 
 $('.space').localScroll(2000);
 $('.nav').localScroll(2000);
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
