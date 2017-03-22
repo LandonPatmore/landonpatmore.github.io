b76c6625c198d50146ceac77fdcdca6e57c3717a
Landon Patmore <landon.patmore@gmail.com>
2017-03-22 00:18:01 -0400

    Things
    -added test chart
    -added colors to sections
    -added scroll back to top feature
    -added back to top button
    -some other things i cant remember

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,45 @@
  * Created by landon on 3/20/17.
  */
 
-$('.space').localScroll(2000);
-$('.nav').localScroll(2000);
+$('.nav').localScroll();
+$('.space').localScroll();
+$('.scrolly').localScroll();
 
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
     }
 });
+
+Morris.Area({
+    element: 'languages',
+    data: [
+        { y: '2006', a: 100, b: 90 },
+        { y: '2007', a: 75,  b: 65 },
+        { y: '2008', a: 50,  b: 40 },
+        { y: '2009', a: 75,  b: 65 },
+        { y: '2010', a: 50,  b: 40 },
+        { y: '2011', a: 75,  b: 65 },
+        { y: '2012', a: 100, b: 90 }
+    ],
+    xkey: 'y',
+    ykeys: ['a', 'b'],
+    labels: ['Series A', 'Series B']
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
